573676bb442dcc6962e7f5853d0cab205a3a47ce
huggycareer <huggycareer@gmail.com>
2024-10-31 17:43:58 +0900

    [feat] BegRepository 생성

---
+++ Backend/kidswallet/src/main/java/com/e201/kidswallet/beg/repository/BegRepository.java
@@ -0,0 +1,8 @@
+package com.e201.kidswallet.beg.repository;
+
+import com.e201.kidswallet.beg.entity.Beg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BegRepository extends JpaRepository<Beg,Long> {
+
+}
