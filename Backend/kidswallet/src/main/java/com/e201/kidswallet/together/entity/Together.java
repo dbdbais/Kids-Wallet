b7c0d4121e7fccd9b11ff8bf54ba8ffad75aa3c8
이강우 <rain5191@naver.com>
2024-10-31 11:40:26 +0900

    [fix] Together id 추가

--- Backend/kidswallet/src/main/java/com/e201/kidswallet/together/entity/Together.java
+++ Backend/kidswallet/src/main/java/com/e201/kidswallet/together/entity/Together.java
@@ -1,4 +1,14 @@
 package com.e201.kidswallet.together.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Together {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }
