ac19ad8ec40b0114c89ce7cf0d13030ab5a27008
huggycareer <huggycareer@gmail.com>
2024-11-03 14:40:10 +0900

    [fix] Builder.Default 추가 및 mission_content,dead_line 컬럼 추가

--- Backend/kidswallet/src/main/java/com/e201/kidswallet/mission/entity/Mission.java
+++ Backend/kidswallet/src/main/java/com/e201/kidswallet/mission/entity/Mission.java
@@ -3,8 +3,10 @@ package com.e201.kidswallet.mission.entity;
 import com.e201.kidswallet.mission.enums.Status;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +14,8 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Table(name = "Mission")
 public class Mission {
 
     @Id
@@ -19,9 +23,10 @@ public class Mission {
     @Column(name="mission_id",nullable = false)
     private Long MissionId;
 
+    @Builder.Default
     @Enumerated(value=EnumType.STRING)
     @Column(name="mission_status",nullable = false)
-    private Status missionStatus;
+    private Status missionStatus=Status.request;
 
     @Column(name="completion_photo",nullable = true)
     private String completionPhoto;
@@ -29,12 +34,17 @@ public class Mission {
     @Column(name="completed_at",nullable = true)
     private LocalDateTime completedAt;
 
+    @CreatedDate
     @Column(name="created_at",nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(name="mission_content",nullable = false)
+    private String missionContent;
+
+    @Column(name="dead_line",nullable = false)
+    private LocalDateTime deadLine;
 
     @OneToOne
     @JoinColumn(name = "beg_id", referencedColumnName = "beg_id")
     private Beg beg;
-
-
 }
