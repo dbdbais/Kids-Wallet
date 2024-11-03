8f94d4da256012af54238e061e69384992e6a94d
huggycareer <huggycareer@gmail.com>
2024-11-03 17:43:42 +0900

    [feat] uploadCompleteImage 메서드 추가

--- Backend/kidswallet/src/main/java/com/e201/kidswallet/mission/repository/MissionRepository.java
+++ Backend/kidswallet/src/main/java/com/e201/kidswallet/mission/repository/MissionRepository.java
@@ -2,6 +2,15 @@ package com.e201.kidswallet.mission.repository;
 
 import com.e201.kidswallet.mission.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission,Long> {
+
+    @Modifying
+    @Query(value="update Mission set completion_photo = :uploadCompleteImage where mission_id= :missionId",nativeQuery = true)
+    public int uploadCompleteImage(@Param("uploadCompleteImage") byte[] uploadCompleteImage, @Param("missionId") Long missionId);
+
+
 }
