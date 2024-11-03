c5e422cdfeb84fff2bae86d328d57763165a57bb
huggycareer <huggycareer@gmail.com>
2024-11-03 17:44:00 +0900

    [feat] assignMission 메서드 추가

--- Backend/kidswallet/src/main/java/com/e201/kidswallet/mission/controller/MissionController.java
+++ Backend/kidswallet/src/main/java/com/e201/kidswallet/mission/controller/MissionController.java
@@ -3,6 +3,7 @@ package com.e201.kidswallet.mission.controller;
 import com.e201.kidswallet.mission.dto.AssignMissionRequestDto;
 import com.e201.kidswallet.mission.dto.BegAcceptRequestDto;
 import com.e201.kidswallet.mission.dto.BeggingRequestDto;
+import com.e201.kidswallet.mission.dto.MissionCompleteRequestDto;
 import com.e201.kidswallet.mission.service.MissionService;
 import com.e201.kidswallet.common.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
@@ -16,31 +17,36 @@ import org.springframework.web.bind.annotation.*;
 public class MissionController {
 
 
-    private final MissionService begsService;
+    private final MissionService service;
 
     @Autowired
     public MissionController(MissionService begsService) {
-        this.begsService = begsService;
+        this.service = begsService;
     }
 
     //TODO: 어느 어른에게 조르기할지 수정
     //아이가 어른에게 조르기
     @PostMapping("/beg")
     public ResponseEntity<?> begging(@RequestBody BeggingRequestDto requestDto){
-        return ResponseDto.response(begsService.begging(requestDto));
+        return ResponseDto.response(service.begging(requestDto));
     }
 
     // 조르기 수락 or 거절
     @PutMapping("/beg")
     public ResponseEntity<?> acceptBegging(@RequestBody BegAcceptRequestDto requestDto){
         log.info(requestDto.toString());
-        return ResponseDto.response(begsService.begAccept(requestDto));
+        return ResponseDto.response(service.begAccept(requestDto));
     }
 
     // 미션 부여
     @PostMapping("/assign")
     public ResponseEntity<?> assignMission(@RequestBody AssignMissionRequestDto requestDto){
-        return ResponseDto.response(begsService.assignMission(requestDto));
+        return ResponseDto.response(service.assignMission(requestDto));
+    }
+
+    @PutMapping("/complete")
+    public ResponseEntity<?>missionComplete(@RequestBody MissionCompleteRequestDto requestDto){
+        return ResponseDto.response(service.uploadCompleteImage(requestDto));
     }
 
 }
