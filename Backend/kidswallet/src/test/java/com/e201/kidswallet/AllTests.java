f0d5a7af6a001c6a7798ad3d8ff758cfe253416e
huggycareer <huggycareer@gmail.com>
2024-11-08 11:10:10 +0900

    [feat] create AllTests

---
+++ Backend/kidswallet/src/test/java/com/e201/kidswallet/AllTests.java
@@ -0,0 +1,11 @@
+package com.e201.kidswallet;
+
+import com.e201.kidswallet.mission.MissionTest;
+import com.e201.kidswallet.user.UserTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ UserTest.class, MissionTest.class })
+public class AllTests {
+}
