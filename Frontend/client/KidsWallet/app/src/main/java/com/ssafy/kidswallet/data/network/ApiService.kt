62570d2882d6dc57c2a6475dcd62bb93c52dbdbb
there2989 <there2989@naver.com>
2024-11-10 18:31:31 +0900

    [feat]: add Login Response Data Code

--- Frontend/client/KidsWallet/app/src/main/java/com/ssafy/kidswallet/data/network/ApiService.kt
+++ Frontend/client/KidsWallet/app/src/main/java/com/ssafy/kidswallet/data/network/ApiService.kt
@@ -1,7 +1,9 @@
 package com.ssafy.kidswallet.data.network
 
+import com.ssafy.kidswallet.data.model.ApiResponse
 import com.ssafy.kidswallet.data.model.LoginModel
 import com.ssafy.kidswallet.data.model.SignUpModel
+import com.ssafy.kidswallet.data.model.UserDataModel
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,5 +13,5 @@ interface ApiService {
     suspend fun registerUser(@Body signUpModel: SignUpModel): Response<Any>
 
     @POST("user/login")
-    suspend fun loginUser(@Body loginModel: LoginModel): Response<Any>
+    suspend fun loginUser(@Body loginModel: LoginModel): Response<ApiResponse>
 }
