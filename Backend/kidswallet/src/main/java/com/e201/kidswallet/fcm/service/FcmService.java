d883169a751a4cedad84d02c72b6d6a01d46f8cb
이강우 <rain5191@naver.com>
2024-11-13 17:02:39 +0900

    [feat] Redis 365일 이상 접속 안하면 초기화 되게 구현 - getToken이 일어날 때마다 TTL 초기화

--- Backend/kidswallet/src/main/java/com/e201/kidswallet/fcm/service/FcmService.java
+++ Backend/kidswallet/src/main/java/com/e201/kidswallet/fcm/service/FcmService.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Slf4j
 @Service
 public class FcmService {
@@ -28,16 +30,19 @@ public class FcmService {
     public StatusCode tokenToRedis(FcmTokenRequestDto requestDto) {
         // Redis 키 구성: transaction:계좌번호:트랜잭션ID
         String key = "fcmToken:" + requestDto.getUserId();
-        fcmRedisTemplate.opsForValue().set(key, requestDto.getTokenValue());
+        fcmRedisTemplate.opsForValue().set(key, requestDto.getTokenValue(), Duration.ofDays(365));
         return StatusCode.SUCCESS;
     }
 
     //get FCMtoken in redis
     public String getToken(long toUserId){
         String key = "fcmToken:" + toUserId;
-        return fcmRedisTemplate.opsForValue().get(key);
-        //없으면 Null
-        //return "cHoQ5ExZTjy6OUjR5lJY19:APA91bG0ZKysLox7BtElV657-5JlK333EMI1s97URrIgMJoiq6e7pCDYr-g-Z6gcYaquQrs3yBwvPmVMjYZyVMFvHOCgFSdnh2VsZUjMJi86zlOlYi20sM8";
+        String token = fcmRedisTemplate.opsForValue().get(key);
+        if (token != null) {
+            // 키가 존재하면 TTL을 1년(365일)로 초기화
+            fcmRedisTemplate.expire(key, Duration.ofDays(365));
+        }
+        return token;
     }
 
     public StatusCode sendMessage(String token, String title, String body) {
